090bea48ef4fa6658c85bf2088438895cd85bdb6
zafar2199clear <zrozmatov0@gmail.com>
2024-01-08 02:00:59 +0500

    feat: add home logout btn

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,15 @@
+import { useAuthContext } from "~context/AuthProvider";
+
 const HomePage = () => {
+    const { logout } = useAuthContext();
     return (
         <div className="home">
             <div className="container">
                 <div className="home__wrap">
                     <h1 className="home__title">Saytga xush keldingiz )</h1>
+                    <button className="home__btn" onClick={logout}>
+                        Login page ga qaytish
+                    </button>
                 </div>
             </div>
         </div>
