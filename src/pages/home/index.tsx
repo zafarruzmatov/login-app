f0b9c1cbfcc6d4ca86858cdec23c4c7bf3fe1e81
zafar2199clear <zrozmatov0@gmail.com>
2024-01-07 23:17:50 +0500

    feat: add home Page UI

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,13 @@
 const HomePage = () => {
-    return <div>HomePage</div>;
+    return (
+        <div className="home">
+            <div className="container">
+                <div className="home__wrap">
+                    <h1 className="home__title">Saytga xush keldingiz )</h1>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default HomePage;
